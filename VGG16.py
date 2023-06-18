5f34f247e945d72a05ee29f35ac477e91c32e9e4
Brightorbit <gipsy674@gmail.com>
2023-06-19 04:37:19 +0900

    특징 추출 테스트(1)

--- VGG16.py
+++ VGG16.py
@@ -14,7 +14,6 @@ from tensorflow.keras.utils import load_img
 from tensorflow.keras.utils import img_to_array
 from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.models import Model
-#from keras.preprocessing.image import load_img
 from scipy.spatial.distance import cosine
 
 # 이미지 디렉토리 경로
@@ -36,9 +35,10 @@ def extract_features(image_path):
     img = np.expand_dims(img, axis=0)  # 배치 차원 추가
     img = preprocess_input(img)  # 이미지 전처리
     features = model.predict(img)  # 특성 추출
+    print(type(features))
     return features.flatten()  # 1차원 벡터로 변환
 
-# # 이미지 특성 추출
+# 이미지 특성 추출
 # features_list = []
 # for image_file in image_files:
 #     image_path = os.path.join(image_directory, image_file)
@@ -47,3 +47,33 @@ def extract_features(image_path):
 
 # # 특성 벡터를 Numpy 배열로 변환
 # features_array = np.array(features_list)
+
+#feature_file = "./features.npy"
+#np.save(feature_file, feature_array)
+
+new_image_path = "./Wine.jpg"
+
+# new_features = extract_features(new_image_path)
+
+# similarities = []
+# for features in features_array:
+#     similarity = 1 - cosine(features, new_features)  # 코사인 유사도 계산
+#     similarities.append(similarity)
+
+# # 유사도가 높은 순으로 정렬하여 상위 5개 인덱스 가져오기
+# top_indices = np.argsort(similarities)[::-1][:5]
+
+# # 유사도가 높은 이미지 파일명 출력
+# for index in top_indices:
+#     print(image_files[index])
+
+fea_li = []
+fe = extract_features(new_image_path)
+fea_li.append(fe)
+print(fea_li)
+   
+unique_elements, counts = np.unique(fea_li, return_counts=True)
+
+# 개수 출력
+for element, count in zip(unique_elements, counts):
+    print(f"{element}: {count}")
