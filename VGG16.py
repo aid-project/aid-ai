a6d951469e981c1916490b284b6d85707c3855f0
Brightorbit <gipsy674@gmail.com>
2023-06-20 11:39:09 +0900

    특징 추출한 npy 생성 및 유사도 검사 코드 작성 중

--- VGG16.py
+++ VGG16.py
@@ -1,6 +1,6 @@
 """
 /**
- * @author [:ee-Yoon-Sang]
+ * @author [Lee-Yoon-Sang]
  * @email [22012081.yu.ac.kr]
  * @create date 2023-06-18 21:49:08
  * @modify date 2023-06-18 21:49:08
@@ -10,11 +10,15 @@
 import os
 import numpy as np
 import h5py
+import math
+import time
 from tensorflow.keras.utils import load_img
 from tensorflow.keras.utils import img_to_array
+from tensorflow.keras.models import load_model
 from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.models import Model
 from scipy.spatial.distance import cosine
+from sklearn.metrics.pairwise import cosine_similarity
 
 # 이미지 디렉토리 경로
 image_directory = "./jpgImage(Transform)/"
@@ -23,10 +27,14 @@ image_directory = "./jpgImage(Transform)/"
 image_files = os.listdir(image_directory)
 
 # VGG16 모델 불러오기
-vgg16 = VGG16(weights='imagenet', include_top=False)
-model = Model(inputs=vgg16.input, outputs=vgg16.get_layer('block4_pool').output)  # 특성 추출할 레이어 설정
+#vgg16 = VGG16(weights='imagenet', include_top=False)
+#model = Model(inputs=vgg16.input, outputs=vgg16.get_layer('block4_pool').output)  # 특성 추출할 레이어 설정
 
 #model.save("VGG16_model.h5") # 저장 후 주석처리할 것.
+
+# 백엔드에서 모델을 불러올 때는 불러와서 compile 하고 해야함.
+model = load_model("./VGG16_model.h5")
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # 이미지 특성 추출 함수
 def extract_features(image_path):
@@ -39,41 +47,62 @@ def extract_features(image_path):
     return features.flatten()  # 1차원 벡터로 변환
 
 # 이미지 특성 추출
-# features_list = []
+features_list = []
+i = 0
+
+# start = time.time()
+
 # for image_file in image_files:
 #     image_path = os.path.join(image_directory, image_file)
 #     features = extract_features(image_path)
 #     features_list.append(features)
+#     print(str(i) + "번째 이미지 특징 추출 완료.")
+#     i = i + 1
+#     pass
+
+# end = time.time()
+
+# print("소요 시간 : " + str(end - start))
 
 # # 특성 벡터를 Numpy 배열로 변환
 # features_array = np.array(features_list)
 
-#feature_file = "./features.npy"
-#np.save(feature_file, feature_array)
+# feature_file = "./features.npy"
+# np.save(feature_file, features_array)
+
+features_array = np.load("./features.npy")
 
 new_image_path = "./Wine.jpg"
 
-# new_features = extract_features(new_image_path)
+new_features = extract_features(new_image_path)
 
-# similarities = []
+print("feature : " + str(features_array.shape))
+print("new : " + str(new_features.shape))
+
+new_features  = np.reshape(new_features, (1,100352))
+
+#similarities = []
+
+similarities = cosine_similarity(features_array, new_features)
+
 # for features in features_array:
 #     similarity = 1 - cosine(features, new_features)  # 코사인 유사도 계산
 #     similarities.append(similarity)
 
-# # 유사도가 높은 순으로 정렬하여 상위 5개 인덱스 가져오기
-# top_indices = np.argsort(similarities)[::-1][:5]
+# 유사도가 높은 순으로 정렬하여 상위 5개 인덱스 가져오기
+top_indices = np.argsort(similarities.flatten())[::-1][:5]
 
-# # 유사도가 높은 이미지 파일명 출력
-# for index in top_indices:
-#     print(image_files[index])
+# 유사도가 높은 이미지 파일명 출력
+for index in top_indices:
+    print(image_files[index])
 
-fea_li = []
-fe = extract_features(new_image_path)
-fea_li.append(fe)
-print(fea_li)
+# fea_li = []
+# fe = extract_features(new_image_path)
+# fea_li.append(fe)
+# print(fea_li)
    
-unique_elements, counts = np.unique(fea_li, return_counts=True)
+# unique_elements, counts = np.unique(fea_li, return_counts=True)
 
 # 개수 출력
-for element, count in zip(unique_elements, counts):
-    print(f"{element}: {count}")
+# for element, count in zip(unique_elements, counts):
+#     print(f"{element}: {count}")
